6f4b25914bb5562505c8db8d310c874ac082cf7b
didadadida93 <didadadida93@gmail.com>
2019-02-02 17:19:23 +0700

    save resources init

---
+++ save_resources.py
@@ -0,0 +1,13 @@
+from utils import extended_login
+
+if __name__ == '__main__':
+    try:
+        email = sys.argv[1]
+        password = sys.argv[2]
+        gameworld_name = sys.argv[3]
+    except IndexError:
+        errmsg = 'Missing arguments\n'+\
+                 f'Usage: \tpython3 {sys.argv[0]} <email> <password> <gameworld>'
+        print(errmsg)
+        sys.exit()
+    gameworld = extended_login(email, password, gameworld_name)
