da5a04fba70fa2c27d5cdbffd541ef7926a81993
didadadida93 <didadadida93@gmail.com>
2019-02-04 11:46:44 +0700

    chickener init project the idea is for finding player who use chicken boot

---
+++ chickener.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    try:
+        email = sys.argv[1]
+        password = sys.argv[2]
+        gameworld_name = sys.argv[3]
+    except IndexError:
+        errmsg = 'Missing arguments\n'+\
+                 f'Usage: \tpython3 {sys.argv[0]} <email> <password> <gameworld>'
+        print(errmsg)
+        sys.exit()
