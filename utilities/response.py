33b579114e4fc8a7739e14f434b00d03b912303a
didadadida93 <didadadida93@gmail.com>
2019-02-16 14:30:05 +0700

    update

--- utilities/response.py
+++ utilities/response.py
@@ -1,26 +1,33 @@
 class Response:
     def __init__(self, response):
-        self._raw = response
-        self._cache()
+        self._response = response
+        self._check_response()
         del response
 
+    def __getattr__(self, key):
+        return self.cache[key]
+
     def __iter__(self):
-        return iter(map(Cache, (d['data'] for d in self.cache)))
+        try:
+            # return iter(map(Cache, (d['data'] for d in self.cache)))
+            return iter(map(Cache, map(lambda d: d['data'], self.cache)))
+        except:
+            return iter(list(self.cache.keys()))
 
     def __repr__(self):
         return str(self.cache)
 
-    def _cache(self):
-        try:
-            self.cache = self._raw['cache'][0]['data']['cache']
-        except:
-            self.cache = self._raw['response']
+    def _check_response(self):
+        if 'cache' in self._response['cache'][0]['data']:
+            self.cache = self._response['cache'][0]['data']['cache']
+        else:
+            self.cache = self._response['response'] or self._response['cache'][0]['data']
 
 
 class Cache:
     """get dict value through attribute"""
     def __init__(self, cache):
-        self.cache = cache
+        self._cache = cache
 
     def __getattr__(self, key):
-        return self.cache[key]
+        return self._cache[key]
