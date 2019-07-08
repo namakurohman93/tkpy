450951536ea6c713e8ae076669eea5454e5f8748
didadadida93 <didadadida93@gmail.com>
2019-07-08 13:19:08 +0700

    update greyer

--- features/greyer.py
+++ features/greyer.py
@@ -3,7 +3,7 @@ import logging
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from tkpy.utilities import credential, login, toggle_farmlist
+from tkpy.utilities import credential, login
 from tkpy.map import Map, distance
 
 logging.basicConfig(
@@ -35,6 +35,10 @@ if __name__ == '__main__':
     logging.info('login')
     driver = login(*credential())
 
+    if driver.plus_account <= 0:
+        logging.info('Need Travian Plus Feature for running this script')
+        sys.exit(0)
+
     logging.info('pulling farmlist data...')
     farmlist = Farmlist(driver)
     farmlist.pull()
@@ -48,14 +52,18 @@ if __name__ == '__main__':
     villageList = [cell for cell in maps.villages if cell['playerId'] in inactivePlayer]
     villages = [village for village in filteredVillage(villageList)]
 
-    for _ in range((len(villages) // 10)+1):
+    if len(villages) == 0:
+        logging.info('no grey village found')
+        sys.exit(0)
+
+    for _ in range((len(villages) // 100)+1):
         n = int(time.time())
         logging.info(f'create farmlist name: {n}')
         farmlist.create_farmlist(n)
         for _ in range(100):
             village = villages.pop()
-            toggleFarmlist(driver, village['villageId'], farmlist[n].id)
-            logging.info(f'add {village.name} to farmlist {n}')
+            toggleFarmlist(driver, village.id, farmlist[n].id)
+            logging.info(f'add {village["village"]["name"]} to farmlist {n}')
             if not villages:
                 break
     logging.info('done')
