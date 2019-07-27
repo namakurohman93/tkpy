1125fec76e6c1ff662f5098caf52666fb8544bb3
didadadida93 <didadadida93@gmail.com>
2019-07-28 05:53:06 +0700

    add setup.py for easy install

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='tkpy',
+    version='0.0.1',
+    description='Travian: Kingdom utilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/didadadida93/tkpy',
+    packages=setuptols.find_packages(
+        exclude=['.tests', '.tests.', 'tests.', 'tests']
+    ),
+    classifier=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT',
+    ],
+)
