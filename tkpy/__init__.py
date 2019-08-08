b28411f57ebc64763d1eecc9b365e02db8eda263
didadadida93 <didadadida93@gmail.com>
2019-08-08 07:33:06 +0700

    reworking on login algorithm

--- tkpy/__init__.py
+++ tkpy/__init__.py
@@ -1,38 +1,17 @@
 from .map import Map
-from .utilities import login as _login
 from .villages import Villages
 from .notepads import Notepad
 from .farmlist import Farmlist
 from .database import DB_DIR
 from .database import get_db
-from .database import get_user_id
-from .database import get_driver
-from .database import update_driver
-from .database import insert_driver
+from .utilities import _login as login
 
 
-def login(email, password, gameworld, avatar=None):
-    user_id = get_user_id(email, password)
-    try:
-        driver, driver_id = get_driver(user_id, gameworld, avatar)
-        try:
-            driver.is_authenticated()
-            driver.update_account()
-        except:
-            # update
-            driver = _login(email, password, gameworld, avatar)
-            update_driver(driver, driver_id)
-    except:
-        driver = _login(email, password, gameworld, avatar)
-        insert_driver(user_id, gameworld, avatar, driver)
-    return driver
-
-def create_db():
-    db = get_db()
-    with open('tkpy/schema.sql', 'r') as f:
-        db.executescript(f.read())
-    db.close()
+def _create_db():
+    with get_db() as db:
+        with open('tkpy/schema.sql', 'r') as f:
+            db.executescript(f.read())
 
 
-if DB_DIR.is_file() is False:
-    create_db()
+if not DB_DIR.is_file():
+    _create_db()
--- tkpy/utilities.py
+++ tkpy/utilities.py
@@ -1,9 +1,13 @@
 import os
+import uuid
+import pickle
 import hashlib
 import binascii
 import argparse
 from .driver import Lobby
-# from .database import CredentialDb
+from .database import get_db
+from .exception import DriverNotFound
+from .exception import NotAuthenticated
 
 
 def credential():
@@ -30,9 +34,8 @@ def credential():
     return email, password, gameworld, avatar
 
 
-# def _login(email, password, gameworld, avatar):
 def login(email, password, gameworld, avatar):
-    """ :func:`_login` internal function for login to gameworld and return
+    """ :func:`login` for login to gameworld and return
     :class:`Gameworld` object.
     """
     lobby = Lobby()
@@ -41,27 +44,8 @@ def login(email, password, gameworld, avatar):
     return client
 
 
-# def login(email, password, gameworld, avatar=None):
-#     """ :func:`login` login interface that used sqlite for storing
-#     :class:`Gameworld` object.
-#     """
-#     db = CredentialDb(email, password, gameworld, avatar)
-#     try:
-#         driver = db.get()
-#         try:
-#             driver.is_authenticated()
-#             driver.update_account()
-#         except:
-#             driver = _login(email, password, gameworld, avatar)
-#             db.update(driver=driver)
-#     except:
-#         driver = _login(email, password, gameworld, avatar)
-#         db.insert(driver=driver)
-#     return driver
-
-
 def hash_password(password):
-    """ Hash a password for storing. """
+    """ Hashing a password. """
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
     pwdhash = hashlib.pbkdf2_hmac(
         'sha512',
@@ -77,6 +61,7 @@ def verify_password(stored_password, provided_password):
     """ Verify a stored password against one provided by user. """
     salt = stored_password[:64]
     stored_password = stored_password[64:]
+
     pwdhash = hashlib.pbkdf2_hmac(
         'sha512',
         provided_password.encode('utf-8'),
@@ -84,4 +69,103 @@ def verify_password(stored_password, provided_password):
         100000
     )
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+
     return pwdhash == stored_password
+
+
+def get_data_from_db(query, params):
+    with get_db() as db:
+        return db.execute(query, params).fetchone()
+
+
+def insert_data_to_db(query, params):
+    with get_db() as db:
+        db.execute(query, params)
+        db.commit()
+
+
+def get_user_id(email, password):
+    user = get_data_from_db(
+        'SELECT * FROM user WHERE email = ?',
+        (email,)
+    )
+
+    if user is None:
+        return user
+
+    if not verify_password(user['password'], password):
+        raise NotAuthenticated('wrong password.')
+
+    return user['id']
+
+
+def add_user(email, password):
+    user_id = uuid.uuid4().hex
+
+    insert_data_to_db(
+        'INSERT INTO user (id, email, password) VALUES (?, ?, ?)',
+        (user_id, email, hash_password(password))
+    )
+
+    return user_id
+
+
+def get_driver_id(user_id, gameworld, avatar):
+    driver = get_data_from_db(
+        'SELECT g.id FROM gameworld g JOIN user u ON g.user_id = u.id WHERE user_id = ? AND avatar = ? AND game_world = ?',
+        (user_id, avatar or '?', gameworld)
+    )
+
+    if driver is None:
+        raise DriverNotFound()
+
+    return driver['id']
+
+
+def insert_driver(user_id, driver, gameworld, avatar):
+    insert_data_to_db(
+        'INSERT INTO gameworld (id, user_id, driver, avatar, game_world) VALUES (?, ?, ?, ?, ?)',
+        (uuid.uuid4().hex, user_id, pickle.dumps(driver), avatar or '?', gameworld)
+    )
+
+
+def update_driver(driver_id, driver):
+    insert_data_to_db(
+        'UPDATE gameworld SET driver = ? WHERE id = ?',
+        (pickle.dumps(driver), driver_id)
+    )
+
+
+def get_driver(driver_id):
+    driver = get_data_from_db(
+        'SELECT driver FROM gameworld WHERE id = ?',
+        (driver_id,)
+    )
+
+    return pickle.loads(driver['driver'])
+
+
+def _login(email, password, gameworld, avatar=None):
+    user_id = get_user_id(email, password) or add_user(email, password)
+
+    try:
+        driver_id = get_driver_id(user_id, gameworld, avatar)
+
+    except DriverNotFound:
+        driver = login(email, password, gameworld, avatar)
+        insert_driver(user_id, driver, gameworld, avatar)
+
+    else:
+        driver = get_driver(driver_id)
+
+        try:
+            driver.is_authenticated()
+
+        except NotAuthenticated:
+            driver = login(email, password, gameworld, avatar)
+            update_driver(driver_id, driver)
+            
+        else:
+            driver.update_account()
+
+    return driver
