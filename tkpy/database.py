47b886d44b736766a886c99b5b49e52a2d78c960
didadadida93 <didadadida93@gmail.com>
2019-08-11 08:33:19 +0700

    update database & schema

--- tkpy/database.py
+++ tkpy/database.py
@@ -3,14 +3,24 @@ import pathlib
 import contextlib
 
 
-DB_DIR = pathlib.Path.home() / 'tkpy-credential.sqlite'
+BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 
 @contextlib.contextmanager
 def get_db():
-    db = sqlite3.connect(DB_DIR, detect_types=sqlite3.PARSE_DECLTYPES)
+    db = sqlite3.connect(
+        BASE_DIR / 'tkpy-credential.sqlite',
+        detect_types=sqlite3.PARSE_DECLTYPES
+    )
     db.row_factory = sqlite3.Row
     try:
         yield db
     finally:
         db.close()
+
+
+def init_db():
+    if not (BASE_DIR / 'tkpy-credential.sqlite').is_file():
+        with get_db() as db:
+            with open(BASE_DIR / 'schema.sql', 'r') as f:
+                db.executescript(f.read())
