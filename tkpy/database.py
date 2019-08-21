6a375decb32542ee377c7f24dd95e9b3d8fb1b10
didadadida93 <didadadida93@gmail.com>
2019-08-22 03:11:18 +0700

    add documentation on database.py

--- tkpy/database.py
+++ tkpy/database.py
@@ -8,9 +8,17 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 @contextlib.contextmanager
 def get_db():
+    """ Context function for get connection to 'tkpy-credential.sqlite'
+    database.
+
+    Usage:
+        >>> with get_db() as db:
+        >>>    user = db.execute('select * from user').fetchall()
+        >>>
+    """
     db = sqlite3.connect(
-        BASE_DIR / 'tkpy-credential.sqlite',
-        detect_types=sqlite3.PARSE_DECLTYPES
+        BASE_DIR / "tkpy-credential.sqlite",
+        detect_types=sqlite3.PARSE_DECLTYPES,
     )
     db.row_factory = sqlite3.Row
     try:
@@ -20,7 +28,9 @@ def get_db():
 
 
 def init_db():
-    if not (BASE_DIR / 'tkpy-credential.sqlite').is_file():
-        with get_db() as db:
-            with open(BASE_DIR / 'schema.sql', 'r') as f:
-                db.executescript(f.read())
+    """ Initialize database for tkpy use. It will overwrite old
+    'tkpy-credential.sqlite' file.
+    """
+    with get_db() as db:
+        with open(BASE_DIR / "schema.sql", "r") as f:
+            db.executescript(f.read())
