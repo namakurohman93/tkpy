d1f5433cd4c34f714ecd9488264267f8374d8973
didadadida93 <didadadida93@gmail.com>
2019-08-25 06:55:18 +0700

    update farmlist

--- tkpy/farmlist.py
+++ tkpy/farmlist.py
@@ -1,4 +1,9 @@
+import dataclasses
+from typing import Any
+
 from .exception import FarmListNotFound
+from .models import ImmutableDataclass
+from .models import ImmutableDict
 
 
 class Farmlist:
@@ -12,40 +17,27 @@ class Farmlist:
         >>> f['Startup farm list']
         <FarmListEntry({'listId': '1631', 'listName': 'Startup farm list', ...})>
     """
+
     def __init__(self, client):
         self.client = client
-        self._raw = dict()
         self.item = dict()
 
     def __getitem__(self, key):
         try:
             return self.item[key]
         except:
-            raise FarmListNotFound(f'{key}')
+            raise FarmListNotFound(f"{key}")
 
     def __repr__(self):
         return str(type(self))
 
     def pull(self):
         """ :meth:`pull` for pulling farmlist data from TK. """
-        self._raw.update(
-            self.client.cache.get({
-                'names': ['Collection:FarmList:']
-            })
-        )
-        # store farmlistEntry object
-        for x in self.raw:
-            self.item[x['listName']] = FarmlistEntry(self.client, x)
-
-    @property
-    def raw(self):
-        """ :property:`raw` is a :func:`generator` that yield raw farmlist
-        data.
-
-        yield: :class:`dict`
-        """
-        for x in self._raw['cache'][0]['data']['cache']:
-            yield x['data']
+        r = self.client.cache.get({"names": ["Collection:Farmlist:"]})
+        for x in r["cache"][0]["data"]["cache"]:
+            self.item[x["data"]["listName"]] = FarmlistEntry(
+                client=self.client, data=x["data"], raw=ImmutableDict()
+            )
 
     @property
     def list(self):
@@ -54,8 +46,8 @@ class Farmlist:
 
         yield: :class:`FarmlistEntry`
         """
-        for x in self.raw:
-            yield FarmlistEntry(self.client, x)
+        for x in self.item.values():
+            yield x
 
     def create_farmlist(self, name):
         """ :meth:`create_farmlist` for create new farmlist.
@@ -64,11 +56,12 @@ class Farmlist:
 
         return: :class:`dict`
         """
-        self.client.farmList.createList({'name': name})
+        self.client.farmList.createList({"name": name})
         self.pull()
 
 
-class FarmlistEntry:
+@dataclasses.dataclass(frozen=True, repr=False)
+class FarmlistEntry(ImmutableDataclass):
     """ :class:`FarmlistEntry` is a represent of farmlist entry. In this
     class contains farmlist entry data. You mostly maintain farmlist entry
     using this class.
@@ -80,47 +73,10 @@ class FarmlistEntry:
         >>> f['Startup farm list'].toggle(536442311)
         >>> f['Startup farm list'].send(536411586)
     """
-    def __init__(self, client, data):
-        self.client = client
-        self.data = data
-        self._raw = dict()
 
-    def __getitem__(self, key):
-        try:
-            return self.data[key]
-        except:
-            raise
-
-    def __repr__(self):
-        return f'<{type(self).__name__}({self.data})>'
-
-    @property
-    def name(self):
-        """ :property:`name` return this farmlist entry name. """
-        return self.data['listName']
-
-    @property
-    def id(self):
-        """ :property:`id` return this farmlist entry id. """
-        return int(self.data['listId'])
-
-    @property
-    def villageIds(self):
-        """ :property:`villageIds` return :class:`list` of village id
-        that stored on this farmlist entry.
-
-        return: :class:`list`
-        """
-        return self.data['villageIds']
-
-    @property
-    def entryIds(self):
-        """ :property:`entryIds` return :class:`list` contains entry id
-        on this farmlist entry.
-
-        return: :class:`list`
-        """
-        return self.data['entryIds']
+    __slots__ = ["client", "raw"]
+    client: Any
+    raw: ImmutableDict
 
     def send(self, villageId):
         """ :meth:`send` for send this farmlist entry from village using
@@ -132,14 +88,13 @@ class FarmlistEntry:
         return: :class:`dict`
         """
         return self.client.troops.startFarmListRaid({
-            'listIds': [self.id],
-            'villageId': villageId
+            "listIds": [self.listId], "villageId": villageId
         })
 
     def _update_data(self, r):
-        for x in r['cache']:
-            if x['name'] == f'FarmList:{self.id}':
-                self.data.update(x['data'])
+        for x in r["cache"]:
+            if x["name"] == f"FarmList:{self.listId}":
+                self.data.update(x["data"])
 
     def add(self, villageId):
         """ :meth:`add` for add village to this farmlist using village id.
@@ -147,11 +102,11 @@ class FarmlistEntry:
         :param villageId: - :class:`int` village id that want to be added
                             to this farmlist entry.
         """
-        r = self.client.farmList.addEntry({
-            'listId': self.id,
-            'villageId': villageId
-        })
-        self._update_data(r)
+        self._update_data(
+            self.client.farmList.addEntry({
+                "listId": self.listId, "villageId": villageId
+            })
+        )
 
     def toggle(self, villageId):
         """ :meth:`toggle` for toggling village to this farmlist entry
@@ -160,19 +115,19 @@ class FarmlistEntry:
         :param villageId: - :class:`int` village id that want to be toggled
                             to this farmlist entry.
         """
-        r = self.client.farmList.toggleEntry({
-            'listId': self.id,
-            'villageId': villageId
-        })
-        self._update_data(r)
+        self._update_data(
+            self.client.farmList.toggleEntry({
+                "listId": self.listId, "villageId": villageId
+            })
+        )
 
     def pull(self):
         """ :meth:`pull` for pulling entry data that exists in this farmlist
         entry.
         """
-        self._raw.update(
+        self.raw.update(
             self.client.cache.get({
-                'names': [f'Collection:FarmListEntry:{self.id}']
+                "names": [f"Collection:FarmListEntry:{self.listId}"]
             })
         )
 
@@ -183,11 +138,12 @@ class FarmlistEntry:
 
         yield: :class:`EntryId`
         """
-        for x in self._raw['cache'][0]['data']['cache']:
-            yield EntryId(self.client, x['data'])
+        for x in self.raw["cache"][0]["data"]["cache"]:
+            yield EntryId(client=self.client, data=x["data"])
 
 
-class EntryId:
+@dataclasses.dataclass(frozen=True, repr=False)
+class EntryId(ImmutableDataclass):
     """ :class:`EntryId` represent of entry from farmlist. This class contains
     more details about entry.
 
@@ -201,44 +157,25 @@ class EntryId:
         536461288
         >>>
     """
-    def __init__(self, client, data):
-        self.client = client
-        self.data = data
 
-    def __repr__(self):
-        return f'<{type(self).__name__}({self.data})>'
-
-    def __getitem__(self, key):
-        try:
-            return self.data[key]
-        except:
-            raise
+    __slots__ = ["client"]
+    client: Any
 
     @property
-    def id(self):
-        """ :property:`id` return id of this entry. """
-        return self.data['entryId']
-
-    @property
-    def villageId(self):
-        """ :property:`id` return village id of this entry. """
-        return self.data['villageId']
-
-    @property
-    def notificationType(self):
+    def notification_type(self):
         """ :property:`notificationType` return notification type of this
         entry.
         """
         try:
-            return self.data['lastReport']['notificationType']
+            return self.data["lastReport"]["notificationType"]
         except:
-            return '0'
+            return "0"
 
     @property
-    def raidedSum(self):
+    def raided_sum(self):
         """ :property:`raidedSum` return raided sum of this entry. """
         try:
-            return self.data['lastReport']['raidedResSum']
+            return self.data["lastReport"]["raidedResSum"]
         except:
             return 0
 
@@ -246,7 +183,7 @@ class EntryId:
     def capacity(self):
         """ :property:`capaciy` return capacity of this entry. """
         try:
-            return self.data['lastReport']['capacity']
+            return self.data["lastReport"]["capacity"]
         except:
             return 0
 
@@ -258,13 +195,11 @@ class EntryId:
                              this entry.
         """
         self.client.farmList.copyEntry({
-            'entryId': self.id,
-            'villageId': self.villageId,
-            'newListId': farmlistId
+            "entryId": self.entryId,
+            "villageId": self.villageId,
+            "newListId": farmlistId,
         })
 
     def delete(self):
         """ :meth:`delete` delete this entry from this farmlist. """
-        self.client.farmList.deleteEntry({
-            'entryId': self.id
-        })
+        self.client.farmList.deleteEntry({"entryId": self.entryId})
