b5471c99fbb898de92bafb8f910d1faf6aff4447
didadadida93 <didadadida93@gmail.com>
2019-08-25 04:18:19 +0700

    update database unittest

--- tests/unit/test_database.py
+++ tests/unit/test_database.py
@@ -1,9 +1,14 @@
 from unittest.mock import Mock, patch
 from tkpy.database import get_db
+from tkpy.database import init_db
 import unittest
 
 
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        with open('./tkpy/schema.sql', 'r') as f:
+            self.schema = f.read()
 
     @patch('tkpy.database.sqlite3')
     @patch('tkpy.database.get_db')
@@ -16,6 +21,13 @@ class Test(unittest.TestCase):
 
         self.assertEqual(foo, 'mocked')
         mock_sqlite.connect().execute.assert_called()
+
+    @patch('tkpy.database.get_db')
+    def test_init_db(self, mock_get_db):
+        init_db()
+        mock_get_db.return_value.__enter__.return_value.executescript.assert_called_with(
+            self.schema
+        )
 
 
 if __name__ == '__main__':
