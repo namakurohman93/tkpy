b9dca965ed85084ddaceca65571dc07fe2afaf95
didadadida93 <didadadida93@gmail.com>
2019-08-26 15:48:00 +0700

    add hero to __init__

--- tkpy/__init__.py
+++ tkpy/__init__.py
@@ -13,6 +13,7 @@ Travian: Kingdom utilities for your need.
 
 
 from .map import Map
+from .hero import Hero
 from .villages import Villages
 from .notepads import Notepad
 from .farmlist import Farmlist
