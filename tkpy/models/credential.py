a6576c481b0cd549dff954e30e657a431a31ea2d
Arief Rohman <didadadida93@gmail.com>
2020-06-06 22:39:57 +0700

    Update credential.py
    * separating `dict_parser` so every class can use it
    * add `Gameworld` class
    * add static method `find_by_email` on `Lobby` class

--- tkpy/models/credential.py
+++ tkpy/models/credential.py
@@ -1,14 +1,13 @@
 from .connection import get_connection
 
+def dict_parser(data):
+    result = {}
+    for key in data.keys():
+        result[key] = data[key]
+
+    return result
+
 class Lobby:
-    @staticmethod
-    def dict_parser(data):
-        result = {}
-        for key in data.keys():
-            result[key] = data[key]
-
-        return result
-
     @staticmethod
     def find_all():
         with get_connection() as conn:
@@ -18,7 +17,7 @@ class Lobby:
             results = cursor.fetchall()
 
             if results:
-                return list(map(Lobby.dict_parser, results))
+                return list(map(dict_parser, results))
             return None
 
     @staticmethod
@@ -30,7 +29,19 @@ class Lobby:
             result = cursor.fetchone()
 
             if result:
-                return Lobby.dict_parser(result)
+                return dict_parser(result)
+            return None
+
+    @staticmethod
+    def find_by_email(email):
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM lobbies WHERE email = ?', (email, ))
+
+            result = cursor.fetchone()
+
+            if result:
+                return dict_parser(result)
             return None
 
     @staticmethod
@@ -40,6 +51,35 @@ class Lobby:
             cursor.execute(
                 'INSERT INTO lobbies (email, password) VALUES (?, ?)',
                 (email, password)
+            )
+            conn.commit()
+
+            return cursor.lastrowid
+
+
+class Gameworld:
+    @staticmethod
+    def find_by_lobby_id_and_gameworld_name(lobby_id, gameworld_name):
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                'SELECT * FROM gameworlds WHERE lobby_id = ? AND gameworld_name = ?',
+                (lobby_id, gameworld_name)
+            )
+
+            result = cursor.fetchone()
+
+            if result:
+                return dict_parser(result)
+            return None
+
+    @staticmethod
+    def create(gameworld_name, driver, lobby_id):
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                'INSERT INTO gameworlds (gameworld_name, driver, lobby_id) VALUES (?, ?, ?)',
+                (gameworld_name, driver, lobby_id)
             )
             conn.commit()
 
