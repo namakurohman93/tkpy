c0072df6002bb8d6373d182fce86a1750a21e542
Arief Rohman <didadadida93@gmail.com>
2020-06-09 15:15:28 +0700

    add new static method `update` on Gameworld model

--- tkpy/models/credential.py
+++ tkpy/models/credential.py
@@ -1,3 +1,6 @@
+import os
+import hashlib
+import binascii
 from .connection import get_connection
 
 def dict_parser(data):
@@ -15,6 +18,35 @@ def query_maker(data):
     return ' AND '.join(temp)
 
 class Lobby:
+    @staticmethod
+    def hash_password(password):
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+
+        pwdhash = hashlib.pbkdf2_hmac(
+            'sha512',
+            password.encode('utf-8'),
+            salt,
+            100000
+        )
+        pwdhash = binascii.hexlify(pwdhash)
+
+        return (salt + pwdhash).decode('ascii')
+
+    @staticmethod
+    def verify_password(hashed_password, password):
+        salt = hashed_password[:64]
+        hashed_password = hashed_password[64:]
+
+        pwdhash = hashlib.pbkdf2_hmac(
+            'sha512',
+            password.encode('utf-8'),
+            salt.encode('ascii'),
+            100000
+        )
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+
+        return pwdhash == hashed_password
+
     @staticmethod
     def find_one(**kwargs):
         query = 'SELECT * FROM lobbies'
@@ -40,7 +72,7 @@ class Lobby:
             cursor = conn.cursor()
             cursor.execute(
                 'INSERT INTO lobbies (email, password) VALUES (?, ?)',
-                (email, password)
+                (email, Lobby.hash_password(password))
             )
             conn.commit()
 
@@ -65,6 +97,26 @@ class Gameworld:
 
             if result:
                 return dict_parser(result)
+            return None
+
+    @staticmethod
+    def update(updater, condition):
+        query = 'UPDATE gameworlds SET '
+        temp = []
+        params = [*list(updater.values()), *list(condition.values())]
+
+        for key in updater.keys():
+            temp.append(f'{key} = ?')
+
+        query += ', '.join(temp)
+
+        query += ' WHERE ' + query_maker(condition)
+
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            conn.commit()
+
             return None
 
     @staticmethod
