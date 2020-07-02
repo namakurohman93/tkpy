c92bada9204c275ed9b2a7f85aed380aeaa44065
Arief Rohman <didadadida93@gmail.com>
2020-07-02 15:49:39 +0700

    add some enum

---
+++ tkpy/enums/tribe.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Tribe(Enum):
+    ROMAN = 1
+    TEUTON = 2
+    GAUL = 3
---
+++ tkpy/enums/troop.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+
+class RomanTroop(Enum):
+    LEGIONNAIRE = "1"
+    PRAETORIAN = "2"
+    IMPERIAN = "3"
+    EQUITES_LEGATI = "4"
+    EQUITES_IMPERATORIS = "5"
+    EQUITES_CAESARIS = "6"
+    BATTERING_RAM = "7"
+    FIRE_CATAPULT = "8"
+    SENATOR = "9"
+    SETTLER = "10"
+    HERO = "11"
+
+
+class TeutonTroop(Enum):
+    CLUBSWINGER = "1"
+    SPEARFIGHTER = "2"
+    AXEFIGHTER = "3"
+    SCOUT = "4"
+    PALADIN = "5"
+    TEUTONIC_KNIGHT = "6"
+    RAM = "7"
+    CATAPULT = "8"
+    CHIEF = "9"
+    SETTLER = "10"
+    HERO = "11"
+
+
+class GaulTroop(Enum):
+    PHALANX = "1"
+    SWORDSMAN = "2"
+    PATHFINDER = "3"
+    THEUTATES_THUNDER = "4"
+    DRUIDRIDER = "5"
+    HAEDUAN = "6"
+    RAM = "7"
+    TREBUCHET = "8"
+    CHIEFTAIN = "9"
+    SETTLER = "10"
+    HERO = "11"
