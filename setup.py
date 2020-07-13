6c3453d759c590831784d3124ed1576849f6380e
Arief Rohman <didadadida93@gmail.com>
2020-07-13 11:43:05 +0700

    impelment black on setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,35 @@
 import setuptools
 
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 
 about = {}
-with open('tkpy/__attrs__.py') as f:
+with open("tkpy/__attrs__.py") as f:
     exec(f.read(), about)
 
 
 setuptools.setup(
-    name=about['__name__'],
-    version=about['__version__'],
-    description=about['__description__'],
+    name=about["__name__"],
+    version=about["__version__"],
+    description=about["__description__"],
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/didadadida93/tkpy',
-    author=about['__author__'],
-    author_email=about['__author_email__'],
-    packages=['tkpy'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/didadadida93/tkpy",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    packages=["tkpy"],
     include_package_data=True,
-    package_data={
-        '': ['schema.sql',],
-    },
-    install_requires=[
-        'requests',
-    ],
-    entry_points={
-        'console_scripts': ['tkpy-init=tkpy.database:init_db'],
-    },
-    license=about['__license__'],
+    package_data={"": ["schema.sql",],},
+    install_requires=["requests",],
+    license=about["__license__"],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
     ],
 )
