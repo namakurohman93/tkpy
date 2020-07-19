b36bbe66ac703ea8c60a7c2f663074e2faec2745
Arief Rohman <didadadida93@gmail.com>
2020-07-19 07:50:09 +0700

    remove unnecesarry commit

--- tkpy/models/__init__.py
+++ tkpy/models/__init__.py
@@ -5,7 +5,6 @@ def flush_tables():
     with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute("DELETE FROM gameworlds")
-        conn.commit()
         cursor.execute("DELETE FROM lobbies")
         conn.commit()
 
