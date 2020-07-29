f1a13c8edfe0902f4b840cc3cb3c20fe015e0b65
didadadida93 <didadadida93@gmail.com>
2020-07-29 11:25:55 +0700

    update testing for map.py

--- tests/unit/test_map.py
+++ tests/unit/test_map.py
@@ -1,5 +1,7 @@
 from tkpy.map import cell_id
+from tkpy.map import distance
 from tkpy.map import reverse_id
+from tkpy.map import slice_map
 from tkpy.map import Map
 import requests_mock
 import unittest
@@ -18,6 +20,35 @@ class TestReverseId(unittest.TestCase):
         self.assertEqual(reverse_id(536920064), (0, 1))
         self.assertNotEqual(reverse_id(536920064), (0, 0))
         self.assertEqual(reverse_id(cell_id(0, -99)), (0, -99))
+
+
+class TestDistance(unittest.TestCase):
+    def testing_distance_function(self):
+        self.assertEqual(distance((0, 0), (0, 3)), 3.0)
+        self.assertNotEqual(distance((0, 0), (0, 3)), 4)
+
+
+class TestingSliceMap(unittest.TestCase):
+    def testing_slice_map_function(self):
+        with open("./tests/unit/fixtures/pickled_driver.py", "rb") as f:
+            g = pickle.load(f)
+
+        with open("./tests/unit/fixtures/map_raw.json", "r") as f:
+            fixtures_map = json.load(f)
+
+        with requests_mock.mock() as mock:
+            mock.register_uri(
+                "POST", "https://com1.kingdoms.com/api/", json=fixtures_map
+            )
+
+            m = Map(g)
+            m.pull()
+
+        nm = slice_map((0, 0), 5, m)
+
+        self.assertEqual(len(list(nm.gen_tiles())), 81)
+        self.assertEqual(len(list(nm.gen_villages())), 13)
+        self.assertEqual(len(list(nm.gen_oases())), 2)
 
 
 class TestMap(unittest.TestCase):
@@ -58,6 +89,8 @@ class TestMap(unittest.TestCase):
         self.assertEqual(len(list(m.gen_abandoned_valley())), 5815)
         self.assertEqual(len(list(m.gen_oases())), 1226)
         self.assertEqual(len(list(m.gen_wilderness())), 26305)
+        self.assertEqual(len(list(m.gen_grey_villages())), 115)
+        self.assertEqual(len(list(m.gen_unoccupied_oases())), 747)
 
         self.assertEqual(m.get_village("village not found?"), {})
         self.assertEqual(
@@ -74,6 +107,7 @@ class TestMap(unittest.TestCase):
 
         self.assertEqual(len(list(m.gen_kingdoms())), 161)
         self.assertEqual(len(list(m.gen_players())), 1624)
+        self.assertEqual(len(list(m.gen_inactive_players())), 114)
 
         self.assertEqual(m.get_kingdom("BLA").id, "100")
         self.assertEqual(m.get_kingdom("BLA").name, "BLA")
@@ -91,6 +125,11 @@ class TestMap(unittest.TestCase):
         self.assertFalse(m.get_player("Mustafa").is_active)
         with self.assertRaises(KeyError):
             m.get_player("Punisher")["adsf"]
+
+        nm = m.slice_map((0, 0), 5)
+        self.assertEqual(len(list(nm.gen_tiles())), 81)
+        self.assertEqual(len(list(nm.gen_villages())), 13)
+        self.assertEqual(len(list(nm.gen_oases())), 2)
 
         with open("./tests/unit/fixtures/hero_equipment.json", "r") as f:
             hero_equipment = json.load(f)
