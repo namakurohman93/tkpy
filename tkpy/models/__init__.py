08ebb07ba3287632f6a8d4e664b98d7c5ba3d1d6
didadadida93 <didadadida93@gmail.com>
2020-08-14 04:47:36 +0700

    add documentation on `flush_tables`

--- tkpy/models/__init__.py
+++ tkpy/models/__init__.py
@@ -2,6 +2,7 @@ from .connection import get_connection
 
 
 def flush_tables():
+    """ `flush_tables` if a function for flushing data from tables. """
     with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute("DELETE FROM gameworlds")
